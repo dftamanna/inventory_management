82ceddd2bd6f83fd1952e7529d928bbfdb6c6e78
Tamanna <dftamanna@gmail.com>
2022-01-25 22:48:28 +0600

    add php

--- left_nav.php
+++ left_nav.php
@@ -4,10 +4,10 @@
     </div>
     <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
   
-    <a href="list_catagory.html"><i class="fa fa-check-square" aria-hidden="true"></i> Catagory</a>
-    <a href="list_item.html"><i class="fa fa-caret-square-o-up"></i> Item</a>
-    <a href="list_supplier.html"><i class="fa fa-cubes"></i> Supplier</a>
-    <a href="list_customer.html"><i  class="fa fa-male"></i> Customer</a>
+    <a href="list_catagory.php"><i class="fa fa-check-square" aria-hidden="true"></i> Catagory</a>
+    <a href="list_item.php"><i class="fa fa-caret-square-o-up"></i> Item</a>
+    <a href="list_supplier.php"><i class="fa fa-cubes"></i> Supplier</a>
+    <a href="list_customer.php"><i  class="fa fa-male"></i> Customer</a>
     <a href="Purchases.html"><i class="fa fa-shopping-cart"></i> Purchases</a>
     <a href="Sale.html"><i  class="fa fa-truck"></i> Sale</a>
    
